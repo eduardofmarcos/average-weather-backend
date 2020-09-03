645c5693c9d9443a591f96cc1430d5e04e3d1e61
eduardofmarcos <eduardomarcos_@outlook.com>
2020-09-03 11:47:38 -0300

    Implemented updated7

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app')
 
 dotenv.config({ path: './config.env' })
 
-const port = process.env.PORT_SERVER || 8080
+const port = process.env.PORT_SERVER || 3000
 
 const server = app.listen(port, () => {
   console.log(`Listening on port ${port}...`)
